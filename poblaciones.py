ff51af9023b5d152636b6cd592893abd9068b985
Arua Ibrahim <aibrahim@abenergia.es>
2015-10-09 11:18:47 +0200

    IMP capitalized poblaciones

--- poblaciones.py
+++ poblaciones.py
@@ -1,59 +1,59 @@
 # -*- coding: utf-8 -*-
 
 links_poblaciones = {
-    'barbastro':    'http://www.aemet.es/xml/municipios/localidad_22048.xml',
-    'binefar':      'http://www.aemet.es/xml/municipios/localidad_22061.xml',
-    'zaragoza':     'http://www.aemet.es/xml/municipios/localidad_50297.xml',
-    'huesca':       'http://www.aemet.es/xml/municipios/localidad_22125.xml',
-    'sitges':       'http://www.aemet.es/xml/municipios/localidad_08270.xml',
-    'monzon':       'http://www.aemet.es/xml/municipios/localidad_22158.xml',
-    'tamarite y tamarite de litera':
+    'Barbastro':    'http://www.aemet.es/xml/municipios/localidad_22048.xml',
+    'Binefar':      'http://www.aemet.es/xml/municipios/localidad_22061.xml',
+    'Zaragoza':     'http://www.aemet.es/xml/municipios/localidad_50297.xml',
+    'Huesca':       'http://www.aemet.es/xml/municipios/localidad_22125.xml',
+    'Sitges':       'http://www.aemet.es/xml/municipios/localidad_08270.xml',
+    'Monzon':       'http://www.aemet.es/xml/municipios/localidad_22158.xml',
+    'Tamarite y Tamarite de Litera':
                     'http://www.aemet.es/xml/municipios/localidad_22225.xml',
-    'ainsa':        'http://www.aemet.es/xml/municipios/localidad_22907.xml',
-    'san esteban de litera':
+    'Ainsa':        'http://www.aemet.es/xml/municipios/localidad_22907.xml',
+    'San Esteban de Litera':
                     'http://www.aemet.es/xml/municipios/localidad_22205.xml',
-    'fraga':        'http://www.aemet.es/xml/municipios/localidad_22112.xml',
-    'barcelona':    'http://www.aemet.es/xml/municipios/localidad_08019.xml',
-    'albelda':      'http://www.aemet.es/xml/municipios/localidad_22009.xml',
-    'altorricon':   'http://www.aemet.es/xml/municipios/localidad_22025.xml',
-    'alqueza':      'http://www.aemet.es/xml/municipios/localidad_22024.xml',
-    'albalate de cinca':
+    'Fraga':        'http://www.aemet.es/xml/municipios/localidad_22112.xml',
+    'Barcelona':    'http://www.aemet.es/xml/municipios/localidad_08019.xml',
+    'Albelda':      'http://www.aemet.es/xml/municipios/localidad_22009.xml',
+    'Altorricon':   'http://www.aemet.es/xml/municipios/localidad_22025.xml',
+    'Alqueza':      'http://www.aemet.es/xml/municipios/localidad_22024.xml',
+    'Albalate de Cinca':
                     'http://www.aemet.es/xml/municipios/localidad_22007.xml',
-    'sant pere de ribes':
+    'Sant Pere de Ribes':
                     'http://www.aemet.es/xml/municipios/localidad_08231.xml',
-    'lleida':       'http://www.aemet.es/xml/municipios/localidad_25120.xml',
-    'pamplona':     'http://www.aemet.es/xml/municipios/localidad_31201.xml',
-    'torredembarra':'http://www.aemet.es/xml/municipios/localidad_43153.xml',
-    'valencia':     'http://www.aemet.es/xml/municipios/localidad_46250.xml',
-    'secastilla':   'http://www.aemet.es/xml/municipios/localidad_22214.xml',
-    'alcolea de cinca':
+    'Lleida':       'http://www.aemet.es/xml/municipios/localidad_25120.xml',
+    'Pamplona':     'http://www.aemet.es/xml/municipios/localidad_31201.xml',
+    'Torredembarra':'http://www.aemet.es/xml/municipios/localidad_43153.xml',
+    'Valencia':     'http://www.aemet.es/xml/municipios/localidad_46250.xml',
+    'Secastilla':   'http://www.aemet.es/xml/municipios/localidad_22214.xml',
+    'Alcolea de Cinca':
                     'http://www.aemet.es/xml/municipios/localidad_22017.xml',
-    'bilbao':       'http://www.aemet.es/xml/municipios/localidad_48020.xml',
-    'benasque':     'http://www.aemet.es/xml/municipios/localidad_22054.xml',
-    'la puebla de castro':
+    'Bilbao':       'http://www.aemet.es/xml/municipios/localidad_48020.xml',
+    'Benasque':     'http://www.aemet.es/xml/municipios/localidad_22054.xml',
+    'La Puebla de Castro':
                     'http://www.aemet.es/xml/municipios/localidad_22187.xml',
-    'vitoria':      'http://www.aemet.es/xml/municipios/localidad_01059.xml',
-    'madrid':       'http://www.aemet.es/xml/municipios/localidad_28079.xml',
-    'monzon':       'http://www.aemet.es/xml/municipios/localidad_22158.xml',
-    'bielsa':       'http://www.aemet.es/xml/municipios/localidad_22057.xml',
-    'binaced':      'http://www.aemet.es/xml/municipios/localidad_22060.xml',
-    'almacelles&almacellas':
+    'Vitoria':      'http://www.aemet.es/xml/municipios/localidad_01059.xml',
+    'Madrid':       'http://www.aemet.es/xml/municipios/localidad_28079.xml',
+    'Monzon':       'http://www.aemet.es/xml/municipios/localidad_22158.xml',
+    'Bielsa':       'http://www.aemet.es/xml/municipios/localidad_22057.xml',
+    'Binaced':      'http://www.aemet.es/xml/municipios/localidad_22060.xml',
+    'Almacelles&Almacellas':
                     'http://www.aemet.es/xml/municipios/localidad_25019.xml',
-    'esplus':       'http://www.aemet.es/xml/municipios/localidad_22099.xml',
-    'logroño':      'http://www.aemet.es/xml/municipios/localidad_26089.xml',
-    'el grado':     'http://www.aemet.es/xml/municipios/localidad_22115.xml',
-    'benabarre':    'http://www.aemet.es/xml/municipios/localidad_22053.xml',
-    'graus':        'http://www.aemet.es/xml/municipios/localidad_22117.xml',
-    'almenar':      'http://www.aemet.es/xml/municipios/localidad_25021.xml',
-    'jaca':         'http://www.aemet.es/xml/municipios/localidad_22130.xml',
-    'binefar':      'http://www.aemet.es/xml/municipios/localidad_22061.xml',
-    'castellon de la plana':
+    'Esplus':       'http://www.aemet.es/xml/municipios/localidad_22099.xml',
+    'Logroño':      'http://www.aemet.es/xml/municipios/localidad_26089.xml',
+    'El grado':     'http://www.aemet.es/xml/municipios/localidad_22115.xml',
+    'Benabarre':    'http://www.aemet.es/xml/municipios/localidad_22053.xml',
+    'Graus':        'http://www.aemet.es/xml/municipios/localidad_22117.xml',
+    'Almenar':      'http://www.aemet.es/xml/municipios/localidad_25021.xml',
+    'Jaca':         'http://www.aemet.es/xml/municipios/localidad_22130.xml',
+    'Binefar':      'http://www.aemet.es/xml/municipios/localidad_22061.xml',
+    'Castellon de la Plana':
                     'http://www.aemet.es/xml/municipios/localidad_12040.xml',
-    'cuarte de huerva':
+    'Cuarte de Huerva':
                     'http://www.aemet.es/xml/municipios/localidad_50089.xml',
-    'sabiñanigo':   'http://www.aemet.es/xml/municipios/localidad_22199.xml',
-    'almudevar':    'http://www.aemet.es/xml/municipios/localidad_22021.xml',
-    'zaidin':       'http://www.aemet.es/xml/municipios/localidad_22254.xml',
-    'naval':        'http://www.aemet.es/xml/municipios/localidad_22160.xml',
-    'estadilla':    'http://www.aemet.es/xml/municipios/localidad_22103.xml'
+    'Sabiñanigo':   'http://www.aemet.es/xml/municipios/localidad_22199.xml',
+    'Almudevar':    'http://www.aemet.es/xml/municipios/localidad_22021.xml',
+    'Zaidin':       'http://www.aemet.es/xml/municipios/localidad_22254.xml',
+    'Naval':        'http://www.aemet.es/xml/municipios/localidad_22160.xml',
+    'Estadilla':    'http://www.aemet.es/xml/municipios/localidad_22103.xml'
 }
